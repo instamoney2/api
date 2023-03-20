de997612e5de207835a7eaefba314a80d301fc31
instamoney2 <125401773+instamoney2@users.noreply.github.com>
2023-03-20 12:39:14 -0300

    new

--- conection.js
+++ conection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
+    host: process.env.PLANETSCALE_DB_HOST,
+    user: process.env.PLANETSCALE_DB_USERNAME,
+    password: process.env.PLANETSCALE_DB_PASSWORD,
+    database: process.env.PLANETSCALE_DB
 })
 
 module.exports = connection;
